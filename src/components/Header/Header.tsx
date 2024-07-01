2fcb05f77545abad55ea44d7a35f9d695a7e57ba
Monica <angulosmonica@gmail.com>
2024-07-01 01:17:06 -0700

    add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css'
+
+export const Header = () => {
+    return(
+        <header className="header">
+                <h1>AluraGeek</h1>
+                <img src="./images/hello-kitty.png" alt="" />
+        </header>
+    );
+};
