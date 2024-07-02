82bc79fd2f69d029007dfc73defb98cbec7e2042
Monica <angulosmonica@gmail.com>
2024-07-01 23:17:20 -0700

    add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Header } from './components/Header/Header.tsx';
-import { Product } from './components/Products/Products.tsx';
-import { AddProductCard } from './components/AddProductCard/AddProductCard.tsx';
+import React, { useState } from "react";
+import { Header } from "./components/Header/Header.tsx";
+import { Product } from "./components/Products/Products.tsx";
+import { AddProductCard } from "./components/AddProductCard/AddProductCard.tsx";
+import { Footer } from "./components/Footer/Footer.tsx";
+import './App.css';
 
-function App() {
+export const App = () => {
+
+  const [products, setProducts] = useState([]);
+  const handleProductAdded = (newProduct) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
   return (
     <div className="App">
-      <Header/>
-      <AddProductCard/>
-      <Product/>
+      <Header />
+      <main className="main__section">
+        <Product />
+        <AddProductCard onProductAdded={handleProductAdded}/>
+      </main>
+      <Footer/>
     </div>
   );
-}
+};
 
 export default App;
