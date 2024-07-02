88846938d46e8b8a698fe937f042737a017e4f88
Monica <angulosmonica@gmail.com>
2024-07-01 23:22:09 -0700

    update class in title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css'
 export const Header = () => {
     return(
         <header className="header">
-                <h1>AluraGeek</h1>
+                <h1 className="alura__geek">AluraGeek</h1>
                 <img src="./images/hello-kitty.png" alt="" />
         </header>
     );
