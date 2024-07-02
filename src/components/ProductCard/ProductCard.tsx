38a8a04fada71e8ef32876e3cf87cba96ea7a4c5
Monica <angulosmonica@gmail.com>
2024-07-01 23:23:07 -0700

    Update names of variables

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import { Trash2 } from "lucide-react";
 import "./ProductCard.css";
 
-export const ProductCard = ({ img, productName, price }) => {
+export const ProductCard = ({id, img, name, price, onDelete }) => {
   return (
     <div className="product__card">
       <figure>
-        <img src={img} alt={productName} />
+        <img src={img} alt={name} />
       </figure>
       <div className="info__container">
         <div>
-          <p>{productName}</p>
+          <p>{name}</p>
           <p>
             $ <span>{price}</span> MNX
           </p>
         </div>
         <div>
-          <Trash2 color="#FDF7F7"/>
+          <Trash2 color="#FDF7F7" className="delete" onClick={() => onDelete(id)}/>
         </div>
       </div>
     </div>
